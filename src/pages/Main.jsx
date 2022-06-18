764eedb08d3b2d0420f85b196414abd34927ea1c
YoujungSon <andn1026@gmail.com>
2022-06-18 10:59:21 +0900

    add.slide

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import Slide from "./Slide";
 
 const Main = () => {
   return (
     <div>
       <Banner />
       <Header />
+      <Slide />
     </div>
   );
 };
