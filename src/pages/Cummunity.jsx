758dec51322bde500c18188efd0706d7c75506a7
YoujungSon <andn1026@gmail.com>
2022-06-20 21:08:55 +0900

    add. post

---
+++ src/pages/Cummunity.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import styled from "styled-components";
+import DetailHeader from "../components/fundDetail/DetailHeader";
+import DetailMenu from "../components/fundDetail/DetailMenu";
+import DetailSideMenu from "../components/DetailSideMenu";
+import Post from "../components/Detail/Post";
+const Cummunity = () => {
+  const fundDetailData = useSelector((state) => state.funding.fundDetail);
+  return (
+    <div>
+      <DetailHeader
+        title={fundDetailData.title}
+        category={fundDetailData.category}
+        img={fundDetailData.imageURL}
+      />
+      <DetailMenu supportersCount={fundDetailData.supportersCount} />
+      <WrapContent>
+        <Post />
+        <DetailSideMenu
+          goal={fundDetailData.fundingGoal}
+          currentFund={fundDetailData.currentFund}
+          expDate={fundDetailData.expDate}
+          supportersCount={fundDetailData.supportersCount}
+          supporters={fundDetailData.supporters}
+          likeCheck={fundDetailData.likeCheck}
+          likeCount={fundDetailData.likeCount}
+        />
+      </WrapContent>
+    </div>
+  );
+};
+
+export default Cummunity;
+const WrapContent = styled.div`
+  margin: 100px 0;
+  display: flex;
+  justify-content: center;
+  gap: 2%;
+  width: 100%;
+`;
--- src/redux/modules/post.js
+++ src/redux/modules/post.js
@@ -1,8 +1,7 @@
 // npx json-server ./db.json --port 5001
-
 import { api } from "../../shared/api";
-import { getCookie } from "../../shared/Cookie";
 
+// actiontype
 const LOAD_POST = "post/LOAD_POST";
 const ADD_POST = "post/ADD_POST";
 const UPDATE_POST = "post/UPDATE_POST";
@@ -11,11 +10,13 @@ const DELETE_POST = "post/DELETE_POST";
 const GET_POST_REQUEST = "post/GET_POST_REQUEST";
 const GET_POST_ERROR = "post/GET_POST_ERROR";
 
+//action함수
 const loadPost = (payload) => ({ type: LOAD_POST, payload });
 const addPost = (payload) => ({ type: ADD_POST, payload });
 const updatePost = (payload) => ({ type: UPDATE_POST, payload });
 const deletePost = (payload) => ({ type: DELETE_POST, payload });
 
+//초기값
 const initialState = {
   list: [],
   post: {},
@@ -23,56 +24,34 @@ const initialState = {
   error: null,
 };
 
-export const __loadPosts = (fundId) => async (dispatch) => {
-  const myToken = getCookie("Authorization");
-
-  const response = await api.get(`/api/fund/${fundId}/comments`, {
-    headers: {
-      Authorization: `Bearer ${myToken}`,
-    },
-  });
-  dispatch(loadPost(response.data));
+//thunk
+export const __loadPosts = (payload) => async (dispatch, getState) => {
+  const data = await api.get("/comment");
+  console.log(data);
+  dispatch(loadPost(data.data));
 };
 
-export const __addPost = (fundId, payload) => async (dispatch) => {
-  const myToken = getCookie("Authorization");
-
-  const data = await api.post(
-    `/api/fund/${fundId}/comment`,
-    {
-      content: payload.content,
-      categori: payload.categori,
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${myToken}`,
-      },
-    }
-  );
+export const __addPost = (payload) => async (dispatch, getState) => {
+  const data = await api.post("/comment", {
+    category: payload.category,
+    content: payload.content,
+  });
+  console.log("이건 데이터", data);
   dispatch(addPost(data.data));
   alert("추가가 완료되었습니다.");
 };
 
-export const __updatePost = (payload, fundId) => async (dispatch) => {
-  const myToken = getCookie("Authorization");
-
-  const request = await api.put(`/api/fund/${fundId}/comment`, payload, {
-    headers: {
-      Authorization: `Bearer ${myToken}`,
-    },
+export const __updatePost = (payload, index) => async (dispatch, getState) => {
+  const request = await api.put("/comment", {
+    category: payload.category,
+    content: payload.content,
   });
   dispatch(updatePost(request.data));
   alert("수정된 완료되었습니다.");
 };
 
-export const __deletePost = (payload, commentId) => async (dispatch) => {
-  const myToken = getCookie("Authorization");
-
-  const msg = await api.delete(`/api/fund/comment/${commentId}`, {
-    headers: {
-      Authorization: `Bearer ${myToken}`,
-    },
-  });
+export const __deletePost = (payload) => async (dispatch, getState) => {
+  const msg = await api.delete("/comment");
   alert(msg.data);
   dispatch(deletePost(payload));
 };
